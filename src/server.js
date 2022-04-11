6446c93e17dcd63b4cf48a8d8b25b7360ca29577
rokas <rokasra@gmail.com>
2022-04-11 15:35:02 +0300

    cao 1 uzd done

--- src/server.js
+++ src/server.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const { port } = require("./config");
-
+const { cars } = require("./db/db");
 const app = express();
+
+//Middleware
+app.use(cors());
+app.use(morgan("dev"));
+
+//CAO 1
+
+app.get("/", (request, response) => {
+  response.json("ok");
+});
+
+app.get("/cars/:model", (request, response) => {
+  const brand = request.params.model;
+  //   console.log("brand===", brand);
+  response.json(cars[brand]);
+});
+
 app.listen(port, () => console.log("express is online", port));
